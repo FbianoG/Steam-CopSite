939ebd9263268586be9384ccefb2b68bb5189307
F.Gonçalves <99352541+FbianoG@users.noreply.github.com>
2023-10-12 16:27:05 -0300

    Mudança nas TagNames e css

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-let apresentation = "https://cdn.akamai.steamstatic.com/steam/clusters/frontpage/b9470f369c925f8d28e3baeb/webm_page_bg_portuguese.webm?t=1696637517" //video de apresentação
-
 let games = [
     {
         name: "overwatch 2", //nome do jogo
         price: 60, //preço real do jogo
         main: true, //diz se o jogo está em destaque
         promo: {
-            type: false, //diz se o jogo está em promoção
+            type: true, //diz se o jogo está em promoção
             value: 10, //porcentagem do desconto da promoção
         },
         images: {
-            src1: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2022/10/OW2-be9287b234afbe7898ac.jpg", //imagem principal do jogo
+            src1: "https://s2-techtudo.glbimg.com/sCGxN12LSMP9G3jXrdscsxoaQt4=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/S/e/cEKYJgSsKInPi4bg6LqA/intro.jpg", //imagem principal do jogo
             src2: "https://assets1.ignimgs.com/2019/09/06/nintendoswitch-overwatchlegendaryedition-screenshot05-1567796447891.png", //imagem secundária do jogo
             src3: "https://www.gamersegames.com.br/wp-content/uploads/2023/05/Overwatch-2-Mes-do-Orgulho-2023-1.jpg", //imagem secundária do jogo
             src4: "https://i.ytimg.com/vi/O161-WNZBP4/maxresdefault.jpg", //imagem secundária do jogo
@@ -23,7 +21,7 @@ let games = [
         main: true,
         promo: {
             type: true,
-            value: 25,
+            value: 20,
         },
         images: {
             src1: "https://t2.tudocdn.net/671739?w=1920",
@@ -38,111 +36,113 @@ let games = [
         price: 79,
         main: true,
         promo: {
-            type: false,
+            type: true,
             value: 65,
         },
         images: {
             src1: "https://images.ctfassets.net/j95d1p8hsuun/29peK2k7Ic6FsPAVjHWs8W/06d3add40b23b20bbff215f6979267e8/NW_OPENGRAPH_1200x630.jpg",
             src2: "https://recreio.uol.com.br/media/_versions/legacy/2021/09/29/new-word-jogo-reune-mais-de-700-mil-jogadores-simultaneos-em-seu-primeiro-dia-1232097_widexl.jpg",
             src3: "https://files.tecnoblog.net/wp-content/uploads/2021/07/new-world-2.jpg",
-            src4: "https://cdn.dfg.com.br/itemimages/921555906-conta-new-word-lvl60-devaloka-1H5P.webp",
-            src5: "https://cdn.akamai.steamstatic.com/steam/apps/1063730/ss_75c46de3e645b812b2750cf8f619fd9dfae2ba69.1920x1080.jpg?t=1695753023",
+            src4: "https://cdn.akamai.steamstatic.com/steam/apps/1063730/ss_fa5a3ac4b5fea25db4b50adbdbcb2ae4e838b3ff.1920x1080.jpg?t=1695753023",
+            src5: "https://s2-techtudo.glbimg.com/ToDKegZNN7XWffCG7l2raI1fO8k=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/o/Z/SNQLe0RFKmlCyz63b7fA/ss12.jpg",
         }
     },
 ]
 
+
+
 //Variáveis
+let videoUrl = "https://cdn.akamai.steamstatic.com/steam/clusters/frontpage/b9470f369c925f8d28e3baeb/webm_page_bg_portuguese.webm?t=1696637517"
 let videoApresentation = document.querySelectorAll('.apresentation video')[0]
-let mainImg = document.querySelectorAll('.recommended img') 
-let recommended = document.querySelectorAll('.recommended')[0]
+let mainImg = document.querySelectorAll('.recommended img')
+let highList = document.querySelectorAll('.highlights-list')[0]
 let nextBtn = document.querySelectorAll('.next')[0]
 let backBtn = document.querySelectorAll('.back')[0]
 let slideCount = 0 //variável para manipular rolagem das imagens em destaque e os "spans"
 
-//Eventos 
 
+
+//Eventos 
 nextBtn.addEventListener('click', slideNext) //evento para passar o jogo em destaque
 backBtn.addEventListener('click', slideBack) //evento para voltar o jogo em destaque
 
 
 
-
 //Functions
-function slideNext() { //function para passar o jogo em destaque
-    let cardsMain = document.querySelectorAll('.card-recommended')
-    let spanCount = document.querySelectorAll('.count-main span')
+function slideNext() { //function para alterar o jogo em destaque
+    let highCard = document.querySelectorAll('.highlights-card')
+    let spanCount = document.querySelectorAll('.highlights-slide-count span')
     slideCount += 1
-    if (slideCount == cardsMain.length) {
+    if (slideCount == highCard.length) {
         slideCount = 0
     }
-    for (let i = 0; i < cardsMain.length; i++) {
+    for (let i = 0; i < highCard.length; i++) {
         if (i == slideCount) {
-            cardsMain[i].style.display = 'flex'
+            highCard[i].style.display = 'flex'
             spanCount[i].style.background = "#999"
         } else {
-            cardsMain[i].style.display = 'none'
+            highCard[i].style.display = 'none'
             spanCount[i].style.background = ""
         }
     }
 }
 
-function slideBack() { //function para voltar o jogo em destaque
-    let cardsMain = document.querySelectorAll('.card-recommended')
-    let spanCount = document.querySelectorAll('.count-main span')
+function slideBack() { //function para alterar o jogo em destaque
+    let highCard = document.querySelectorAll('.highlights-card')
+    let spanCount = document.querySelectorAll('.highlights-slide-count span')
     slideCount -= 1
     if (slideCount < 0) {
-        slideCount = cardsMain.length - 1
+        slideCount = highCard.length - 1
     }
-    for (let i = 0; i < cardsMain.length; i++) {
+    for (let i = 0; i < highCard.length; i++) {
         if (i == slideCount) {
-            cardsMain[i].style.display = 'flex'
+            highCard[i].style.display = 'flex'
             spanCount[i].style.background = "#999"
         } else {
-            cardsMain[i].style.display = 'none'
+            highCard[i].style.display = 'none'
             spanCount[i].style.background = ""
         }
     }
 }
 
 function createData() {
-    videoApresentation.src = `${apresentation}` //manipular vídeo de apresentação
-
-    let gamesMain = games.filter(element =>{ //filtra  [{games]} e retorna todos os jogos que {"main:" "true"}
+    videoApresentation.src = videoUrl //manipular vídeo de apresentação
+    let gamesMain = games.filter(element => { //filtra [{games}] e retorna todos os jogos que {"main:" "true"}
         return element.main == true
     })
-    gamesMain.forEach(element => { //cria dinamicamente os elementos do [{gamesMain]}
+    gamesMain.forEach(element => { //cria dinamicamente os elementos do [{gamesMain}]
         let newSpanCount = document.createElement('span')
-        document.querySelectorAll('.count-main')[0].appendChild(newSpanCount)
+        document.querySelectorAll('.highlights-slide-count')[0].appendChild(newSpanCount)
         let newCard = document.createElement('div')
-        newCard.classList = "card-recommended"
+        newCard.classList = "highlights-card"
         newCard.innerHTML = `
-            <div class="recommended-image">
+            <div class="highlights-card-image">
                 <img src="${element.images.src1}" alt="">
             </div>
-            <div class="recommended-data">
+            <div class="highlights-card-data">
                 <h2>${element.name}</h2>
-                <div class="mini-image">
+                <div class="highlights-card-data-images">
                     <img src="${element.images.src2}" alt="">
                     <img src="${element.images.src3}" alt="">
                     <img src="${element.images.src4}" alt="">
                     <img src="${element.images.src5}" alt="">
                 </div>
-                <div class="labels">
+                <div class="highlights-card-data-labels">
                     <p>Já disponível</p>
                     <label>Popular</label>
                 </div>
-                <div class="recommended-price">
-                ${element.promo.type == true && element.promo.value > 0 ? //operador ternário para verificar se o jogo está ou não em promoção
-                    `
-                    <div class="promo">
-                        <p id="promoPerc">${element.promo.value}%</p>
-                        <div class="price">
-                            <p class="price-old">R$ ${element.price.toFixed(2)}</p>
-                            <p id="price-end">R$ ${(element.price - element.promo.value).toFixed(2)}</p>
-                        </div>
-                    </div>` : `<p>R$ ${element.price.toFixed(2)}</p>`
-                }
-                
+                <div class="highlights-card-data-price">
+                    ${element.promo.type == true && element.promo.value > 0 ? //operador ternário para verificar se o jogo está ou não em promoção
+                `
+                        <div class="highlights-card-data-price-promo">
+                            <p id="promoPerc">${element.promo.value}%</p>
+                            <div class="highlights-card-data-price-promo-price">
+                                <p class="price-old">R$ ${element.price.toFixed(2)}</p>
+                                <p id="price-end">R$ ${(element.price - element.promo.value).toFixed(2)}</p>
+                            </div>
+                        </div>` : `<p>R$ ${element.price.toFixed(2)}</p>
+                        `
+            }
                     <span>
                         <i class="fa-brands fa-windows"></i>
                         <i class="fa-brands fa-apple"></i>
@@ -151,15 +151,15 @@ function createData() {
                 </div>
             </div>
         `
-        recommended.appendChild(newCard)
+        highList.appendChild(newCard)
     });
-
-    let cardsMain = document.querySelectorAll('.card-recommended')
-    let spanCount = document.querySelectorAll('.count-main span')
-    cardsMain[slideCount].style.display = 'flex' //display=none nos jogos subsequente ao index "slideCount"
+    let highCard = document.querySelectorAll('.highlights-card')
+    let spanCount = document.querySelectorAll('.highlights-slide-count span')
+    highCard[slideCount].style.display = 'flex' //display=none nos jogos subsequente ao index "slideCount"
     spanCount[slideCount].style.background = "#999" //muda background do "span[0]"
-
 }
 
-//Chamados
+
+
+// Chamadas
 createData() //chama a função para filtrar e criar dinamicamente os elementos
